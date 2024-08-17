ab7a7fbe35f8ae4af84818f9d41e83e8c5a234bf
Daniel Vilela <daniel_2bueno@hotmail.com>
2024-08-17 20:37:24 -0300

    - added unit test to employee controller - added two features test to employee feature

---
+++ backend/tests/Feature/EmployeeFeaturesTest.php
@@ -0,0 +1,269 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+class EmployeeFeaturesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testListEmployee(): void
+    {
+        //Arrange
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*'] 
+        );
+
+        $employeeData = [
+            'name' => 'Daniel', 
+            'middle_name' => 'Developer', 
+            'last_name' => 'Da Silva', 
+            'cpf' => '51185410007',
+            'birth_date' => '1995-05-03',
+            'comorbidity' => false
+        ];
+
+        //arrange
+        $response = $this->postJson('/api/employee', $employeeData);
+        
+        //arrange
+        $response->assertStatus(201)
+        ->assertJsonStructure([
+            'data' => [
+                'name',
+                'middle_name',
+                'last_name',
+                'cpf',
+                'birth_date',
+                'comorbidity',
+                'id',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+
+        //test
+        $response = $this->getJson('/api/employee');
+        $expectedStructure = [
+            'data' => [
+                [
+                    'id',
+                    'cpf',
+                    'name',
+                    'middle_name',
+                    'last_name',
+                    'birth_date',
+                    'comorbidity',
+                    'deleted_at',
+                    'created_at',
+                    'updated_at'
+                ]
+            ],
+            'links' => [
+                'first',
+                'last',
+                'prev',
+                'next'
+            ],
+            'meta' => [
+                'current_page',
+                'from',
+                'last_page',
+                'links' => [
+                    [
+                        'url',
+                        'label',
+                        'active'
+                    ]
+                ],
+                'path',
+                'per_page',
+                'to',
+                'total'
+            ]
+        ];
+
+        $response->assertStatus(200)
+        ->assertJsonStructure($expectedStructure);
+    }
+
+    public function testCreateEmployeeSuccess(): void
+    {
+        //Arrange
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*'] 
+        );
+
+        $employeeData = [
+            'name' => 'Daniel', 
+            'middle_name' => 'Developer', 
+            'last_name' => 'Da Silva', 
+            'cpf' => '51185410007',
+            'birth_date' => '1995-05-03',
+            'comorbidity' => false
+        ];
+
+        //act
+        $response = $this->postJson('/api/employee', $employeeData);
+        
+        //test
+        $response->assertStatus(201)
+        ->assertJsonStructure([
+            'data' => [
+                'name',
+                'middle_name',
+                'last_name',
+                'cpf',
+                'birth_date',
+                'comorbidity',
+                'id',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    public function testCreateEmployeeFail(): void
+    {
+        //Arrange
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*'] 
+        );
+
+        $employeeWrongData = [
+            'name' => 'Daniel', 
+            'middle_name' => 'Developer', 
+            'last_name' => 'Da Silva', 
+            'cpf' => '51185410007',
+        ];
+
+        //act
+        $response = $this->postJson('/api/employee', $employeeWrongData);
+        
+        //assert. 
+        //I'd rather test the structure than the exacly body response, 
+        //'cause if we are treating with multi language response it would be simplified and safe
+        $response->assertStatus(422)
+        ->assertJsonStructure([
+            'message',
+            'errors' => [
+                'birth_date',
+                'comorbidity',
+            ],
+        ]);
+    }
+
+
+
+    public function testUpdateEmployeeSuccess(): void
+    {
+        //Arrange
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*'] 
+        );
+
+        $employeeData = [
+            'name' => 'Daniel', 
+            'middle_name' => 'Developer', 
+            'last_name' => 'Da Silva', 
+            'cpf' => '51185410007',
+            'birth_date' => '1995-05-03',
+            'comorbidity' => false
+        ];
+
+        //Arrange
+        $response = $this->postJson('/api/employee', $employeeData);
+        
+        //Arrange
+        $response->assertStatus(201)
+        ->assertJsonStructure([
+            'data' => [
+                'name',
+                'middle_name',
+                'last_name',
+                'cpf',
+                'birth_date',
+                'comorbidity',
+                'id',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        //arrange
+        $employeeToUpdate = json_decode($response->getContent());
+
+
+        //act
+        $employeNewName = [
+            'name' => 'AlteredName', 
+        ];
+        $response = $this->putJson("/api/employee/{$employeeToUpdate->data->id}", $employeNewName);
+
+        //assert
+        $response->assertStatus(200)
+                 ->assertJsonFragment($employeNewName);
+    }
+
+
+
+    public function testDeleteEmployeeSuccess(): void
+    {
+        //Arrange
+        $user = User::factory()->create();
+        Sanctum::actingAs(
+            $user,
+            ['*'] 
+        );
+
+        $employeeData = [
+            'name' => 'Daniel', 
+            'middle_name' => 'Developer', 
+            'last_name' => 'Da Silva', 
+            'cpf' => '51185410007',
+            'birth_date' => '1995-05-03',
+            'comorbidity' => false
+        ];
+
+        //Arrange
+        $response = $this->postJson('/api/employee', $employeeData);
+        
+        //Arrange
+        $response->assertStatus(201)
+        ->assertJsonStructure([
+            'data' => [
+                'name',
+                'middle_name',
+                'last_name',
+                'cpf',
+                'birth_date',
+                'comorbidity',
+                'id',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+
+        //arrange
+        $employeeToDelete = json_decode($response->getContent());
+
+
+        //act
+        $response = $this->DeleteJson("/api/employee/{$employeeToDelete->data->id}");
+
+        $response->assertStatus(204);
+    }
+}
---
+++ backend/tests/Unit/EmployeeControllerTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\EmployeeController;
+use App\Http\Requests\Employee\StoreRequest;
+use App\Http\Resources\EmployeeResource;
+use App\Models\Employee;
+use App\Services\EmployeeService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Mockery;
+use Tests\TestCase;
+
+class EmployeeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $employeeService;
+    protected $controller;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        ////Arrange -Service mock
+        $this->employeeService = Mockery::mock(EmployeeService::class);
+        
+        //Arrange -controller instance
+        $this->controller = new EmployeeController($this->employeeService);
+    }
+
+    public function testStoreEmployeeSuccess()
+    {
+        // data to mock
+        $employeeData = [
+            'name' => 'Daniel', 
+            'middle_name' => 'Developer', 
+            'last_name' => 'Da Silva', 
+            'cpf' => '51185410007',
+            'birth_date' => '1995-05-03',
+            'comorbidity' => false
+        ];
+
+        //Arrange - mock Model Employee
+        $employee = Mockery::mock(Employee::class);
+        $employee->shouldReceive('toArray')->andReturn($employeeData);
+
+
+        //Arrange -mock employee service
+        $this->employeeService->shouldReceive('store')
+            ->once()
+            ->with($employeeData)
+            ->andReturn($employee);
+
+        //Arrange -  mock EmployeeResource
+        $employeeResource = Mockery::mock(EmployeeResource::class, function ($mock) use ($employee) {
+            $mock->shouldReceive('resolve')->andReturn($employee->toArray());
+        });
+
+        //Arrange: StoreRequest Mock
+        $request = Mockery::mock(StoreRequest::class);
+        $request->shouldReceive('validated')->andReturn($employeeData);
+
+        //act
+        $response = $this->controller->store($request);
+
+        $this->assertInstanceOf(EmployeeResource::class, $response);
+
+        //assert
+        $this->assertEquals($employeeResource->resolve(), $response->resolve());
+    }
+
+    public function testStoreEmployeeFailure()
+    {
+        //mock prepare
+        $this->employeeService->shouldReceive('store')
+            ->once()
+            ->andThrow(new \Exception('Error storing employee'));
+
+        $employeeData = [
+            'name' => 'Daniel', 
+            'middle_name' => 'Developer', 
+            'last_name' => 'Da Silva', 
+            'cpf' => '51185410007',
+            'birth_date' => '1995-05-03',
+            ];
+
+        //mock prepare
+        $request = Mockery::mock(StoreRequest::class);
+        $request->shouldReceive('validated')->andReturn($employeeData);
+
+        //unit testing
+        $response = $this->controller->store($request);
+
+        $expectedJson = [
+            "message" => "O campo comorbidity é obrigatório.",
+            "errors" => [
+                "comorbidity" => [
+                    "O campo comorbidity é obrigatório."
+                ]
+            ]
+        ];
+
+        //asserting
+        $this->assertEquals(422, $response->status());
+        $this->assertJson($response->getContent(), json_encode($expectedJson));
+
+    }
+
+}
