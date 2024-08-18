94d6e98083132d2b096abbaf82b4cfb552b5a90a
Daniel Vilela <daniel_2bueno@hotmail.com>
2024-08-17 21:13:37 -0300

    - migration + model

---
+++ backend/app/Models/Vaccine.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UsesUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Vaccine extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    use UsesUuid;
+
+    protected $guarded = [];
+}
